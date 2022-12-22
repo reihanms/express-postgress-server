9bd00e3d75709464f17660f7a0fe54cc04423b28
Reihan Manzis <reihanmanzis1@gmail.com>
2022-12-22 07:05:00 +0700

    fix: cors

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(cors({
-    origin : 'http://localhost:3000',
-    methods : ["GET","POST"],
-}))
-
 connectDB();
 
 app.use(bodyParser.json());
